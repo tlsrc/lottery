9344ce6de932ec051e7430b771071840191d5afe
tristanles <tristanlescut@gmail.com>
2013-07-21 15:58:35 -0400

    Application skeleton with maven for eclipse

---
+++ src/test/java/org/tristanles/LotteryTest.java
@@ -0,0 +1,16 @@
+package org.tristanles;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LotteryTest {
+	
+	@org.junit.Test
+	public void emptyTest() {
+		assertTrue(true);
+	}
+
+}
